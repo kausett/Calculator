b1e59f7079185258d140cbe7314c8a25318a4ab5
kokusanya3 <okusanya.kayode@gmail.com>
2018-05-07 18:21:23 -0500

    Modified UI to support potrate and landscape

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisply = display.text!
+            display.text = textCurrentlyInDisply + digit
+        }else{
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue: Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    
+    private var Brain = CalcularorBrain()
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            Brain.setOperand(displayValue)
+        }
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            Brain.performOperator(mathematicalSymbol)
+        }
+        if let result = Brain.result {
+            displayValue = result
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
+    
 }
 
